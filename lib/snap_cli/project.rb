3a4501671ab5704b945853e920cd986c591c8bc6
Lawrance Shepstone <lawrance@collabcollective.com>
2021-08-15 20:51:07 +0100

    Adds remove_review_url to effort, project and task commands

--- lib/snap_cli/project.rb
+++ lib/snap_cli/project.rb
@@ -8,5 +8,12 @@ module SnapCLI
     def update_review_url(project_id, url)
       SnapCLI.api_client.project_resource.update_review_url(project_id, url)
     end
+
+    desc 'remove_review_url PROJECT_ID',
+         'Removes the review URL for a given project'
+
+    def remove_review_url(project_id)
+      SnapCLI.api_client.project_resource.update_review_url(project_id, nil)
+    end
   end
 end
--- lib/snap_cli/task.rb
+++ lib/snap_cli/task.rb
@@ -8,5 +8,12 @@ module SnapCLI
     def update_review_url(task_id, url)
       SnapCLI.api_client.card_resource.update_review_url(task_id, url)
     end
+
+    desc 'remove_review_url TASK_ID',
+         'Removes the review URL for a given task'
+
+    def remove_review_url(task_id)
+      SnapCLI.api_client.card_resource.update_review_url(task_id, nil)
+    end
   end
 end
